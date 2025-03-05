0841c3ca97dc7b987bf3f132d7626001563dab00
HN_Linux <Hnelamir@gmail.cm>
2025-03-05 23:41:08 +0200

    Finalizing the project

--- 01_Part_1_Grad/src/main.cpp
+++ 01_Part_1_Grad/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <array>
 #include "math.hpp"
 
 int main(){
     //std :: cout << "Hello"<<"\n";
     std:: cout<<max::maximum(10,50,30,310.20,2550.206)<<"\n";
     std:: cout<<min::minimum(10.30,5,30,310.20,2550.206)<<"\n";
-    std:: cout<<diff::sub(10.30,5,30,310.20,2550.206)<<"\n";
+    std:: cout<<diff::sub(10.30,5,30,310.20,2550.206)<<std::endl;
 }
