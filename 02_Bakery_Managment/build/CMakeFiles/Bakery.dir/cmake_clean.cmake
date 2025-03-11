9c9fbc8df207c5fb9171502ace5d5b5ea1b66f07
HN_Linux <Hnelamir@gmail.cm>
2025-03-11 17:10:57 +0200

    writing first line of code and configure Cmake

---
+++ 02_Bakery_Managment/build/CMakeFiles/Bakery.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Bakery"
+  "Bakery.pdb"
+  "CMakeFiles/Bakery.dir/src/main.cpp.o"
+  "CMakeFiles/Bakery.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bakery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
